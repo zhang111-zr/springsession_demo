3386286e36dcab4446b73f4f76c9cef148e9ec43
my-java <1506246081@qq.com>
2020-11-10 09:18:49 +0800

    Initial commitqqqqqqqqqqqqqq

---
+++ src/test/java/com/offcn/springsession_demo/SpringsessionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offcn.springsession_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsessionDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
