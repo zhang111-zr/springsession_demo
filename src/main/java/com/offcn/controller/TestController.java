11b6cb5a2f26e370fe2aff6cf6d1f556debb184e
YeLong <1508198094@qq.com>
2020-11-10 21:21:35 +0800

    这是我的第一次修改代码

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -1,7 +1,6 @@
 package com.offcn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
@@ -10,6 +9,7 @@ import javax.servlet.http.HttpSession;
 public class TestController {
     @GetMapping("/set")
     public String setSession(HttpSession session){
+        System.out.println("这是我的第一次修改代码---叶龙");
         session.setAttribute("msg","Hello");
         return "OK";
     }
